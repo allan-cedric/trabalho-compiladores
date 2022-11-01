1b37788e60ab295bf25c002f38ecd22c59f27ec9
allan-cedric <allan.py3000@gmail.com>
2022-11-01 06:56:17 -0300

    add: init files

---
+++ compilador.h
@@ -0,0 +1,47 @@
+/* -------------------------------------------------------------------
+ *            Arquivo: compilador.h
+ * -------------------------------------------------------------------
+ *              Autor: Bruno Muller Junior
+ *               Data: 08/2007
+ *      Atualizado em: [09/08/2020, 19h:01m]
+ *
+ * -------------------------------------------------------------------
+ *
+ * Tipos, protótipos e variáveis globais do compilador (via extern)
+ *
+ * ------------------------------------------------------------------- */
+
+#define TAM_TOKEN 16
+
+typedef enum simbolos {
+  simb_program, simb_var, simb_begin, simb_end,
+  simb_identificador, simb_numero,
+  simb_ponto, simb_virgula, simb_ponto_e_virgula, simb_dois_pontos,
+  simb_atribuicao, simb_abre_parenteses, simb_fecha_parenteses,
+
+  simb_rotulo, simb_tipo, simb_vetor, simb_de, simb_procedimento,
+  simb_funcao, simb_pular, simb_se, simb_entao, simb_senao,
+  simb_enquanto, simb_faca, simb_adicao, simb_subtracao, simb_multiplicacao,
+  simb_divisao, simb_nao, simb_e, simb_ou, simb_abre_colchetes, simb_fecha_colchetes
+} simbolos;
+
+
+
+/* -------------------------------------------------------------------
+ * variáveis globais
+ * ------------------------------------------------------------------- */
+
+extern simbolos simbolo, relacao;
+extern char token[TAM_TOKEN];
+extern int nivel_lexico;
+extern int desloc;
+extern int nl;
+
+
+/* -------------------------------------------------------------------
+ * prototipos globais
+ * ------------------------------------------------------------------- */
+
+void geraCodigo (char*, char*);
+int yylex();
+void yyerror(const char *s);
